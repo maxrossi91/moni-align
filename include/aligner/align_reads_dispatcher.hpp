44c77196d45d814226ca3ae1fd0c1fb794c9f450
maxrossi91 <maxrossi91@gmail.com>
2023-02-18 17:27:59 -0500

    Add dots print when processing 10000 reads

--- include/aligner/align_reads_dispatcher.hpp
+++ include/aligner/align_reads_dispatcher.hpp
@@ -177,8 +177,8 @@ void *mt_align_worker(void *param)
       else
       {
         stats += p->aligner->align(b, sam_fd);
+        if(stats.processed_reads % 10000 == 0) std::cout << ".";
       }
-      if(stats.processed_reads % 10000 == 0) std::cout << ".";
     }
 
     // Finalize the learned reads if there are no reads left.
@@ -329,8 +329,8 @@ statistics_t st_align(aligner_t *aligner, std::string pattern_filename, std::str
         if (aligner->align(&b->buf[i], sam_fd))
           stats.aligned_reads++;
         stats.processed_reads++;
+        if(stats.processed_reads % 10000 == 0) std::cout << ".";
       }
-      if(stats.processed_reads % 10000 == 0) std::cout << ".";
     }
     kbseq_destroy(b);
   } 
@@ -363,8 +363,8 @@ statistics_t st_align(aligner_t *aligner, std::string pattern_filename, std::str
       else
       {
         stats += aligner->align(b,sam_fd);
+        if(stats.processed_reads % 10000 == 0) std::cout << ".";
       }
-      if(stats.processed_reads % 10000 == 0) std::cout << ".";
     }
     if ((stats.processed_reads - pn_reads) > 1000000) {
       verbose("Number of processed reads: ", stats.processed_reads);
