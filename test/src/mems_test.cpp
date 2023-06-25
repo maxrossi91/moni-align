6acf2caa06c8eb8e9f37bcd17ff6139eaa47b856
Rahul Varki <rvarki@login5.ufhpc>
2023-06-24 23:49:06 -0400

    Made MEM test into a function and added soft check whether a MEM occurs at read mapping position

--- test/src/mems_test.cpp
+++ test/src/mems_test.cpp
@@ -1,4 +1,4 @@
-/* mems_test - Check number of MEMs generated
+/* mems_test - Check number of MEMs generated and MEM occurances
     Copyright (C) 2020 Massimiliano Rossi
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
 */
 /*!
    \file mems_test.cpp
-   \brief mems_test.cpp Check number of MEMs generated.
+   \brief mems_test.cpp Check number of MEMs generated and MEM occurances.
    \author Rahul Varki
    \date 13/6/2023
 */
@@ -35,13 +35,48 @@
 //*********************** Global Variables *********************************
 std::string test_dir = "../../../data/reads/";
 std::string index_prefix = "../../../data/index/Chr21.10";
-std::string filename_mate1 = "Chr21.10.HG002.R1.fastq.gz";
-std::string filename_mate2 = "Chr21.10.HG002.R2.fastq.gz";
-int unique_mems[10] = {37, 27, 24, 17, 18, 12, 36, 16, 36, 12};
+std::string filename_mate1 = "Chr21.15.HG002.R1.fastq.gz";
+std::string filename_mate2 = "Chr21.15.HG002.R2.fastq.gz";
+size_t unique_mems_25[15] = {37, 27, 24, 17, 18, 38, 12, 36, 17, 36, 36, 28, 16, 36, 12};
+size_t unique_mems_50[15] = {14, 8, 8, 6, 4, 12, 4, 12, 6, 12, 12, 12, 4, 10, 4};
+size_t unique_mems_100[15] = {2, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 1, 1};
+std::tuple<size_t, size_t> read_pos[15] = {{5252411,5252185},
+{17759023,17758869},
+{14155431,14155216},
+{35172095,35171902},
+{31636331,31636593},
+{27627199,27627410},
+{44912613,44912848},
+{46302597,46302774},
+{33240110,33239881},
+{45379122,45379336},
+{33966004,33966181},
+{46111841,46111661},
+{46302812,46303003},
+{45746487,45746652},
+{33322802,33322976}};
+
+// Read List
+// simulated.1
+// simulated.2
+// simulated.3
+// simulated.4
+// simulated.5
+// simulated.80742
+// simulated.100173
+// simulated.130880
+// simulated.193280
+// simulated.209923
+// simulated.290674
+// simulated.320211
+// simulated.531955
+// simulated.660211
+// simulated.793077
+
 //*********************** Testing chaining algorithm ************************
 
-TEST_CASE("MEM Testing", "[mems]")
-{   
+void mem_test(size_t min_len, size_t mems[15])
+{
     kseq_t *mate1 = nullptr;
     kseq_t *mate2 = nullptr;
 
@@ -62,11 +97,11 @@ TEST_CASE("MEM Testing", "[mems]")
     mate1 = kseq_init(fp_mate1);
     mate2 = kseq_init(fp_mate2);
     kpbseq_t *b = kpbseq_init();
-    size_t b_size = 10; //Using 2 results in seg fault
+    size_t b_size = 15; //Using 2 results in seg fault
     size_t l = 0;
     l = kpbseq_read(b, mate1, mate2, b_size);
     REQUIRE(l == b_size);
-   
+
     std::vector<std::vector<aligner<seed_finder<plain_slp_t, ms_pointers<>>>::paired_alignment_t>> alignments;
     std::vector<kpbseq_t *> memo;
 
@@ -75,8 +110,8 @@ TEST_CASE("MEM Testing", "[mems]")
     alignments.push_back(std::vector<aligner<seed_finder<plain_slp_t, ms_pointers<>>>::paired_alignment_t>(l));
     kpbseq_t *batch = memo.back();
 
-    verbose("Initializing the MEM finder object");
-    seed_finder<plain_slp_t, ms_pointers<>> mem_finder = seed_finder<plain_slp_t, ms_pointers<>>(index_prefix, 25, false, 5000); //Have to provide the types for template class and functions
+    verbose("Initializing the MEM finder object with min seed length of ", min_len);
+    seed_finder<plain_slp_t, ms_pointers<>> mem_finder = seed_finder<plain_slp_t, ms_pointers<>>(index_prefix, min_len, false, 5000); //Have to provide the types for template class and functions
 
     int num_reads = batch->mate1->l;
     for (size_t i = 0; i < num_reads; ++i)
@@ -89,7 +124,32 @@ TEST_CASE("MEM Testing", "[mems]")
         mem_finder.find_seeds(alignment.mate2, alignment.mems, 0, MATE_2 | MATE_F);
         mem_finder.find_seeds(&alignment.mate2_rev, alignment.mems, alignment.mate1->seq.l, MATE_2 | MATE_RC);
         verbose("Found this many unique MEMs: ", alignment.mems.size());
-        REQUIRE(alignment.mems.size() == unique_mems[i]);
+        REQUIRE(alignment.mems.size() == mems[i]);
+        verbose("Checking whether MEM occurs at true read mapping position");
+        bool check1 = false;
+        bool check2 = false;
+        for (size_t j = 0; j < alignment.mems.size(); ++j){
+            for (size_t k = 0; k < alignment.mems[j].occs.size(); ++k){
+                if (alignment.mems[j].occs[k] == std::get<0>(read_pos[i])){
+                    check1 = true;
+                }
+                if (alignment.mems[j].occs[k] == std::get<1>(read_pos[i])){
+                    check2 = true;
+                }
+            }
+        }
+
+        if (check1)
+            verbose("Found MEM at mate 1's truth position");
+        else
+            verbose("Did not find MEM at mate 1's truth position");
+        
+        if (check2)
+            verbose("Found MEM at mate 2's truth position");
+        else
+            verbose("Did not find MEM at mate 2's truth position");
+
+        verbose("*************************************************");
     }
 
     //Call the destructors 
@@ -98,7 +158,13 @@ TEST_CASE("MEM Testing", "[mems]")
     kseq_destroy(mate2);
     gzclose(fp_mate1);
     gzclose(fp_mate2);
+}
 
+TEST_CASE("MEM Testing", "[mems]")
+{   
+    mem_test(25,unique_mems_25);
+    mem_test(50,unique_mems_50);
+    mem_test(100,unique_mems_100);
     bool finish = true;
     REQUIRE(finish);
 }
