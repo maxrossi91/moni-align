6b12a7567c3d56bb1d69bfb10ad84c5b5aa4506f
Rahul Varki <rvarki@login7.ufhpc>
2024-02-12 18:46:05 -0500

    Additional functions to support writing MEMs to SAM

--- include/common/kpbseq.h
+++ include/common/kpbseq.h
@@ -174,6 +174,28 @@ inline static void free_kseq_t(kseq_t *s)
     free_kstring_t(s->seq);
     free_kstring_t(s->qual);
 }
+
+inline static void copy_partial_kstring_t(kstring_t &l, kstring_t &r, size_t start_pos, size_t len)
+{
+    l.l = len;
+    l.m = r.m;
+    l.s = (char *)malloc(l.m);
+    for (size_t i = 0; i < len; ++i)
+        l.s[i] = r.s[start_pos + i];
+    if (l.m > l.l)
+      l.s[l.l] = 0;
+}
+
+inline static void copy_partial_kseq_t(kseq_t *l, kseq_t *r, size_t start_pos, size_t len)
+{
+    copy_kstring_t(l->name, r->name);
+    copy_kstring_t(l->comment, r->comment);
+    copy_partial_kstring_t(l->seq, r->seq, start_pos, len);
+    copy_partial_kstring_t(l->qual, r->qual, start_pos, len);
+    //l->last_char = r->last_char;
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
